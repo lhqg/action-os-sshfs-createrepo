639f3cefe48116a366602368d186fea3208d0afb
Hubert Quarantel-Colombani <hubert@quarantel.name>
2024-11-01 15:07:09 +0100

    Make sure the EPEL repo is available

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,14 @@ ARG PLATFORM=amd64
 FROM --platform=linux/${PLATFORM} ${DISTRIBUTION}:${DISTRO_VERSN}
 
 # set the environment variables that gha sets
+ENV DISTRO_VERSN=9
 ENV INPUT_SFTP_SERVER=""
 ENV INPUT_SFTP_USER=""
 ENV INPUT_SFTP_REMOTE_PATH=""
 
 # Install build environment
-RUN dnf install -y createrepo fuse-sshfs
+RUN dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${DISTRO_VERSN}.noarch.rpm
+RUN dnf install -y createrepo fuse fuse-sshfs
 
 COPY ./build.sh .
 
--- build.sh
+++ build.sh
@@ -26,40 +26,47 @@ touch $LOCKFILE
 
 pwd
 
-[ -n "${SFTP_PASSWORD}" ] && echo "${SFTP_PASSWORD}" > ${WORKDIR}/.sftp_password
-
-if [ -r ${WORKDIR}/.sftp_password ]
+if [ -n "${INPUT_SFTP_PASSWORD}" ]
 then
-  echo "::notice title=CreateRepodata::SFTP server      is ${SFTP_SERVER}"
-  echo "::notice title=CreateRepodata::SFTP user        is ${SFTP_USER}"
-  echo "::notice title=CreateRepodata::SFTP remote path is ${SFTP_REMOTE_PATH}"
+  echo "::notice title=CreateRepodata::Using SFTP password from input paramaters"
+  echo "${INPUT_SFTP_PASSWORD}" > ${WORKDIR}/.sftp_password
+fi
+
+if [ -r ${WORKDIR}/.sftp_password -a -s ${WORKDIR}/.sftp_password ]
+then
+  echo "::notice title=CreateRepodata::SFTP server      is ${INPUT_SFTP_SERVER}"
+  echo "::notice title=CreateRepodata::SFTP user        is ${INPUT_SFTP_USER}"
+  echo "::notice title=CreateRepodata::SFTP remote path is ${INPUT_SFTP_REMOTE_PATH}"
 
   # Add SSH key of SFTP server to known hosts
-  if [ -n "${SFTP_SERVER_KEY_TYPE}" -a -n "${SFTP_SERVER_KEY_FINGERPRINT}" ]
+  if [ -n "${INPUT_SFTP_SERVER_KEY_TYPE}" -a -n "${INPUT_SFTP_SERVER_KEY_FINGERPRINT}" ]
   then
 
     mkdir -p ~/.ssh
     chown 0750 ~/.ssh
-    echo "${SFTP_SERVER} ${SFTP_SERVER_KEY_TYPE} ${SFTP_SERVER_KEY_FINGERPRINT}" >> ~/.ssh/known_hosts
+    echo "::notice title=CreateRepodata::Adding SSH host key to known hosts"
+    echo "${INPUT_SFTP_SERVER} ${INPUT_SFTP_SERVER_KEY_TYPE} ${INPUT_SFTP_SERVER_KEY_FINGERPRINT}" >> ~/.ssh/known_hosts
     chmod 0600 ~/.ssh/known_hosts
 
     # Mount the SFTP remote path
     YUMREPO_DIR=${WORKDIR}/yumrepo
     mkdir -m 0755 ${YUMREPO_DIR}
 
-    cat ${WORKDIR}/.sftp_password | sshfs ${SFTP_USER}@${SFTP_SERVER}:${SFTP_REMOTE_PATH} ${YUMREPO_DIR}
+    echo "::notice title=CreateRepodata::Trying to perform mount on ${YUMREPO_DIR}"
+    cat ${WORKDIR}/.sftp_password | sshfs -o password_stdin ${INPUT_SFTP_USER}@${INPUT_SFTP_SERVER}:${INPUT_SFTP_REMOTE_PATH} ${YUMREPO_DIR}
     RC=$?
 
     if [ $RC -eq 0 ]
     then
-      echo "::notice title=CreateRepodata::Running createrepo for ${SFTP_REMOTE_PATH}"
+      echo "::notice title=CreateRepodata::Running createrepo for ${INPUT_SFTP_REMOTE_PATH}"
       createrepo -v ${YUMREPO_DIR}
       RC=$?
     else
       echo "::error title=CreateRepodata::Unable to perform SSHFS mount"
     fi
 
-    fusermount -u ${YUMREPO_DIR}
+    cd ${WORKDIR}
+    fusermount -u ${YUMREPO_DIR} > /dev/null 2>&1
   else
     echo "::error title=CreateRepodata::No SSH key type or fingerprint."
   fi
